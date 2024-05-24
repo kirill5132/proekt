15f7ce9c4fc64bec8864e0abc1481f951f68ee0c
USER PC3 <sn0020068@gmail.com>
2024-05-24 18:13:38 +0300

    gi

--- main.py
+++ main.py
@@ -1,44 +1,42 @@
-from telegram import Bot, Update
-from telegram import Updater, CommandHandler, MessageHandler, Filters
+
+import telebot
 import requests
 from bs4 import BeautifulSoup
-
+from typing import Optional
 
 TOKEN = '6961538103:AAHAEnYPauTwon9Bh4yWCuwA5rR4ewxQIlw'
+bot = telebot.TeleBot(TOKEN)
 
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Привет! Я могу парсить сайты для тебя. Прежде чем приступить отправь мне сайт "
+                                      "в виде https://example.com")
 
-def start(update, context):
-    update.message.reply_text('Привет! Я могу парсить сайты для тебя.')
+@bot.message_handler(commands=['help'])
+def helping(message):
+    bot.send_message(message.chat.id, "Чтобы начать, просто отправьте мне URL сайта.")
 
+@bot.message_handler(content_types=['text'])
+def parse_site(message):
+    url = message.text
+    if not url.startswith('http'):
+        bot.send_message(message.chat.id, 'Пожалуйста, отправьте URL в формате https://example.com')
+        return
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        title = soup.find('title').text
+        script = soup.find('script').text
+        paragraphs = soup.find_all('p')
+        parsed_text = f"Заголовок: {title}\n\n"
+        for paragraph in paragraphs:
+            parsed_text += f"{paragraph.text}\n\n"
+        parsed_text = f"Заголовок: {script}\n\n"
+        for paragraph in paragraphs:
+            parsed_text += f"{paragraph.text}\n\n"
+        bot.send_message(message.chat.id, parsed_text)
+        bot.send_message(message.chat.id, 'Сайт успешно отпарсен!')
+    except Exception as e:
+        bot.send_message(message.chat.id, 'Ошибка при парсинге сайта: {}'.format(e))
 
-def help(update, context):
-    update.message.reply_text('Чтобы начать, просто отправьте мне URL сайта.')
-
-
-def parse_site(update, context):
-    url = 'http://example.com'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    title = soup.find('title').text
-    paragraphs = soup.find_all('p')
-    for paragraph in paragraphs:
-        print(paragraph.text)
-
-
-    update.message.reply_text('Сайт успешно отпарсен.')
-
-
-def error_handler(update, context):
-    print(f'Update {update} caused error {context.error}')
-
-
-updater = Updater(TOKEN)
-dp = updater.dispatcher
-
-dp.add_handler(CommandHandler('start', start))
-dp.add_handler(CommandHandler('help', help))
-dp.add_handler(MessageHandler(Filters.text & ~Filters.command, parse_site))
-dp.add_error_handler(error_handler)
-
-updater.start_polling()
-updater.idle()
+bot.infinity_polling()
